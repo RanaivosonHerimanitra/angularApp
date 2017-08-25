8505f17811df4b67a7c8c685acf8bc15e8375e5b
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2017-08-24 22:18:32 -0400

    add properties

--- src/app/sandbox.component.ts
+++ src/app/sandbox.component.ts
@@ -1,11 +1,33 @@
 import {Component} from '@angular/core';
 @Component({
     selector: 'sandbox',
-    template: `<h1>Hello {{name}}</h1>`
+    template: `
+    <h1>{{name}} is {{age}} years old</h1>
+    <h2>my name is {{person.firstname}} {{person.lastname}}</h2>
+    <ul>
+    <li>{{showAge()}}</li>
+    </ul>
+    `
 
 })
 
 export class SandboxComponent {
-    name="Herimanitra";
-
+    name= "Herimanitra";
+    age= 35;
+    person = {
+        firstname: 'steve',
+        lastname: 'mayer'
+    }
+    constructor()
+    {
+        console.log("demarrage du serveur, on peut tout init par le constructeur...")
+        this.hasbirthday();
+    }
+    hasbirthday(){
+        this.age += 1;
+    }
+    showAge()
+    {
+        return this.age;
+    }
 }
