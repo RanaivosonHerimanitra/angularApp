aea1928a94461444b681c089aea3c0a8279c7219
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2017-09-05 18:28:24 -0400

    using angular router basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { SandboxComponent } from './sandbox/sandbox.component';
 import {MyservicesService} from './services/myservices.service';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+
+const appRoutes:Routes = [
+
+{path:'',component:HomeComponent},
+{path:'about',component:AboutComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    SandboxComponent
+    SandboxComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [MyservicesService],
   bootstrap: [AppComponent]
